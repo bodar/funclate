59a2b89fd0da5cb749d09afb52d42f478760f26b
Daniel Worthington-Bodart <dan@bodar.com>
2011-08-20 19:51:07 +0100

    Json: ignores whitespace around values

--- src/com/googlecode/funclate/json/grammar/Grammar.java
+++ src/com/googlecode/funclate/json/grammar/Grammar.java
@@ -2,26 +2,28 @@ package com.googlecode.funclate.json.grammar;
 
 import com.googlecode.totallylazy.Maps;
 import com.googlecode.totallylazy.Pair;
-import com.googlecode.totallylazy.numbers.Numbers;
 import org.codehaus.jparsec.Parser;
 import org.codehaus.jparsec.Parsers;
 import org.codehaus.jparsec.Scanners;
 import org.codehaus.jparsec.functors.Map;
 import org.codehaus.jparsec.functors.Tuple3;
+import org.codehaus.jparsec.pattern.CharPredicates;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.codehaus.jparsec.Scanners.isChar;
-import static org.codehaus.jparsec.Scanners.notChar;
 import static org.codehaus.jparsec.Scanners.string;
 
 public class Grammar {
-    public static final Parser<Void> OPTIONAL_WHITESPACE = Scanners.WHITESPACES.optional();
-    public static final Parser<Void> SEPARATOR = wsChar(',');
+    public static final Parser<Void> IGNORE_WHITESPACE = isChar(CharPredicates.IS_WHITESPACE).skipMany();
 
     private static Parser<Void> wsChar(final char character) {
-        return Parsers.between(OPTIONAL_WHITESPACE, isChar(character), OPTIONAL_WHITESPACE);
+        return ws(isChar(character));
+    }
+
+    private static <T> Parser<T> ws(final Parser<T> parser) {
+        return Parsers.between(IGNORE_WHITESPACE, parser, IGNORE_WHITESPACE);
     }
 
     public static final Parser<Void> NULL = string("null");
@@ -54,6 +56,8 @@ public class Grammar {
         }
     });
 
+    public static final Parser<Void> SEPARATOR = wsChar(',');
+
     public static final Parser<List<Object>> ARRAY = Parsers.between(wsChar('['), VALUE.sepBy(SEPARATOR), wsChar(']'));
 
     public static final Parser<java.util.Map<String, Object>> OBJECT = Parsers.between(wsChar('{'), PAIR.sepBy(SEPARATOR), wsChar('}')).map(new Map<List<Pair<String, Object>>, java.util.Map<String, Object>>() {
@@ -63,6 +67,6 @@ public class Grammar {
     });
 
     static {
-        value.set(Parsers.<Object>or(OBJECT, ARRAY, STRING, NUMBER, BOOLEAN, NULL));
+        value.set(ws(Parsers.<Object>or(OBJECT, ARRAY, STRING, NUMBER, BOOLEAN, NULL)));
     }
 }
