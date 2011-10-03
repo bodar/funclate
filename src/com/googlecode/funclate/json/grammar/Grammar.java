1001e76244b1b037627fbcc9913578a669339ad2
Daniel Worthington-Bodart <dan@bodar.com>
2011-10-03 20:55:36 +0100

    Switched to lazyparsec

--- src/com/googlecode/funclate/json/grammar/Grammar.java
+++ src/com/googlecode/funclate/json/grammar/Grammar.java
@@ -1,23 +1,23 @@
 package com.googlecode.funclate.json.grammar;
 
+import com.googlecode.totallylazy.Callable1;
 import com.googlecode.totallylazy.Maps;
 import com.googlecode.totallylazy.Pair;
-import org.codehaus.jparsec.Parser;
-import org.codehaus.jparsec.Parsers;
-import org.codehaus.jparsec.Scanners;
-import org.codehaus.jparsec.functors.Map;
-import org.codehaus.jparsec.functors.Tuple3;
-import org.codehaus.jparsec.pattern.CharPredicates;
+import com.googlecode.lazyparsec.Parser;
+import com.googlecode.lazyparsec.Parsers;
+import com.googlecode.lazyparsec.Scanners;
+import com.googlecode.lazyparsec.pattern.CharacterPredicates;
+import com.googlecode.totallylazy.Triple;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static com.googlecode.totallylazy.Sequences.characters;
-import static org.codehaus.jparsec.Scanners.isChar;
-import static org.codehaus.jparsec.Scanners.string;
+import static com.googlecode.lazyparsec.Scanners.isChar;
+import static com.googlecode.lazyparsec.Scanners.string;
 
 public class Grammar {
-    public static final Parser<Void> IGNORE_WHITESPACE = isChar(CharPredicates.IS_WHITESPACE).skipMany();
+    public static final Parser<Void> IGNORE_WHITESPACE = isChar(CharacterPredicates.IS_WHITESPACE).skipMany();
 
     private static Parser<Void> wsChar(final char character) {
         return ws(isChar(character));
@@ -29,20 +29,20 @@ public class Grammar {
 
     public static final Parser<Void> NULL = string("null");
 
-    public static final Parser<Boolean> BOOLEAN = string("true").or(string("false")).source().map(new Map<String, Boolean>() {
-        public Boolean map(String s) {
+    public static final Parser<Boolean> BOOLEAN = string("true").or(string("false")).source().map(new Callable1<String, Boolean>() {
+        public Boolean call(String s) {
             return Boolean.valueOf(s);
         }
     });
 
-    public static final Parser<String> STRING = Scanners.DOUBLE_QUOTE_STRING.map(new Map<String, String>() {
-        public String map(String withQuotes) {
+    public static final Parser<String> STRING = Scanners.DOUBLE_QUOTE_STRING.map(new Callable1<String, String>() {
+        public String call(String withQuotes) {
             return characters(withQuotes).tail().init().toString("", "", "", Integer.MAX_VALUE);
         }
     });
 
-    public static final Parser<Number> NUMBER = Scanners.DECIMAL.map(new Map<String, Number>() {
-        public Number map(String s) {
+    public static final Parser<Number> NUMBER = Scanners.DECIMAL.map(new Callable1<String, Number>() {
+        public Number call(String s) {
             return new BigDecimal(s);
         }
     });
@@ -50,9 +50,9 @@ public class Grammar {
     private static final Parser.Reference<Object> value = Parser.newReference();
     public static final Parser<Object> VALUE = value.lazy();
 
-    public static final Parser<Pair<String, Object>> PAIR = Parsers.tuple(STRING, wsChar(':'), VALUE).map(new Map<Tuple3<String, Void, Object>, Pair<String, Object>>() {
-        public Pair<String, Object> map(Tuple3<String, Void, Object> triple) {
-            return Pair.pair(triple.a, triple.c);
+    public static final Parser<Pair<String, Object>> PAIR = Parsers.tuple(STRING, wsChar(':'), VALUE).map(new Callable1<Triple<String, Void, Object>, Pair<String, Object>>() {
+        public Pair<String, Object> call(Triple<String, Void, Object> triple) {
+            return Pair.pair(triple.first(), triple.third());
         }
     });
 
@@ -60,8 +60,8 @@ public class Grammar {
 
     public static final Parser<List<Object>> ARRAY = Parsers.between(wsChar('['), VALUE.sepBy(SEPARATOR), wsChar(']'));
 
-    public static final Parser<java.util.Map<String, Object>> OBJECT = Parsers.between(wsChar('{'), PAIR.sepBy(SEPARATOR), wsChar('}')).map(new Map<List<Pair<String, Object>>, java.util.Map<String, Object>>() {
-        public java.util.Map<String, Object> map(List<Pair<String, Object>> pairs) {
+    public static final Parser<java.util.Map<String, Object>> OBJECT = Parsers.between(wsChar('{'), PAIR.sepBy(SEPARATOR), wsChar('}')).map(new Callable1<List<Pair<String, Object>>, java.util.Map<String, Object>>() {
+        public java.util.Map<String, Object> call(List<Pair<String, Object>> pairs) {
             return Maps.map(pairs);
         }
     });
