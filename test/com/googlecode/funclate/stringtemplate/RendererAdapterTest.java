7213ede2871576ddcb8978146948e78899e2eddb
dan <dan@bodar.com>
2011-11-03 15:26:42 +0000

    Breaking Change: Changed 'format' support to use seperate Renderers/Callables

--- test/com/googlecode/funclate/stringtemplate/RendererAdapterTest.java
+++ test/com/googlecode/funclate/stringtemplate/RendererAdapterTest.java
@@ -3,27 +3,28 @@ package com.googlecode.funclate.stringtemplate;
 import com.googlecode.funclate.Renderer;
 import org.junit.Test;
 
+import static com.googlecode.funclate.stringtemplate.EnhancedStringTemplateGroup.defaultEncoders;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class RendererAdapterTest {
     @Test
     public void supportsDifferentFormatsForAnyWrappedRenderer() throws Exception{
-        RendererAdapter adapter = new RendererAdapter(returns());
+        RendererAdapter adapter = new RendererAdapter(returns(), defaultEncoders());
         assertThat(adapter.toString("<root/>", "xml"), is("&lt;root/&gt;"));
         assertThat(adapter.toString("<root/>", "html"), is("&lt;root/&gt;"));
         assertThat(adapter.toString("Some values", "url"), is("Some+values"));
-        assertThat(new RendererAdapter(callingThrows()).toString("Some values", "raw"), is("Some values"));
+        assertThat(new RendererAdapter(callingThrows(), defaultEncoders()).toString("Some values", "raw"), is("Some values"));
     }
 
     @Test
     public void supportsChainingFormats() throws Exception{
-        assertThat(new RendererAdapter(callingThrows()).toString("1 < 2", "raw,html"), is("1 &lt; 2"));
+        assertThat(new RendererAdapter(callingThrows(), defaultEncoders()).toString("1 < 2", "raw,html"), is("1 &lt; 2"));
     }
 
     @Test
     public void supportsEmptyFormat() throws Exception{
-        assertThat(new RendererAdapter(returns()).toString("1 < 2", ""), is("1 < 2"));
+        assertThat(new RendererAdapter(returns(), defaultEncoders()).toString("1 < 2", ""), is("1 < 2"));
     }
 
     private Renderer<Object> callingThrows() {
