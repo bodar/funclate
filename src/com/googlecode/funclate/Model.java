04650117b34f550123dfe6a596ca35355404c09b
dan <dan@bodar.com>
2012-02-08 11:12:17 +0000

    Updated libs

--- src/com/googlecode/funclate/Model.java
+++ src/com/googlecode/funclate/Model.java
@@ -5,7 +5,6 @@ import com.googlecode.totallylazy.Callable1;
 import com.googlecode.totallylazy.Callable2;
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.Sequences;
-import com.googlecode.totallylazy.records.Keyword;
 
 import java.io.FileReader;
 import java.util.ArrayList;
