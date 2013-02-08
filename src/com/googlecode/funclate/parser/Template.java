b5d72c6b6394a59cdb4f92cf28e1daecf5ff9baa
dan, stu and phill <dan, stu and phill@localhost>
2013-02-08 15:14:45 +0000

    Refactoring of Grammar, made attributes and text values work correctly

--- src/com/googlecode/funclate/parser/Template.java
+++ src/com/googlecode/funclate/parser/Template.java
@@ -2,6 +2,7 @@ package com.googlecode.funclate.parser;
 
 import com.googlecode.funclate.Renderer;
 import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Mapper;
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.Strings;
 
@@ -17,10 +18,7 @@ public class Template implements Renderer<Map<String, Object>> {
     }
 
     public String render(final Map<String, Object> map) throws Exception {
-        return objects.map(new Callable1<Renderer<Map<String, Object>>, Object>() {
-            public Object call(Renderer<Map<String, Object>> renderer) throws Exception {
-                return renderer.render(map);
-            }
-        }).toString(Strings.EMPTY);
+        return objects.map(functions.render(map)).toString(Strings.EMPTY);
     }
+
 }
