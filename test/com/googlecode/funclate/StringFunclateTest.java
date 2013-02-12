5470d0bd19eb0ced5de5d22987226f723593836d
stu and phill <stu and phill@localhost>
2013-02-12 14:37:22 +0000

    2 argument functions

--- test/com/googlecode/funclate/StringFunclateTest.java
+++ test/com/googlecode/funclate/StringFunclateTest.java
@@ -1,5 +1,6 @@
 package com.googlecode.funclate;
 
+import com.googlecode.totallylazy.Callable2;
 import com.googlecode.totallylazy.Predicates;
 import com.googlecode.totallylazy.UnaryFunction;
 import org.hamcrest.Matchers;
@@ -20,10 +21,25 @@ public class StringFunclateTest {
                 return "Hello " + s;
             }
         }));
+
         StringFunclate funclate = new StringFunclate("$myTemplate(\"bar.height\")$", parent);
 
         Model model = model().add("dan", "Dan");
         assertThat(funclate.render(model), is("Hello bar.height"));
+    }
+
+    @Test
+    public void canExecuteFunctionsWith2Arguments() throws Exception {
+        Funclate parent = new CompositeFunclate().add("add", Predicates.always(), StringFunclate.functions.both(new Callable2<String, String, String>() {
+            @Override
+            public String call(String s, String s2) throws Exception {
+                return "" + (Integer.parseInt(s) + Integer.parseInt(s2));
+            }
+        }));
+
+        StringFunclate funclate = new StringFunclate("$add(\"1\", \"2\")$", parent);
+        Model model = model();
+        assertThat(funclate.render(model), is("3"));
     }
 
     @Test
