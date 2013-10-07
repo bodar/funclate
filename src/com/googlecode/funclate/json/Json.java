c7647229d730c857ee5c650004fdc4654b48239c
Daniel Worthington-Bodart <dan@bodar.com>
2013-10-07 12:08:28 +0100

    Updated JCompilo and cleaned up Json support

--- src/com/googlecode/funclate/json/Json.java
+++ src/com/googlecode/funclate/json/Json.java
@@ -3,7 +3,7 @@ package com.googlecode.funclate.json;
 import com.googlecode.funclate.Model;
 import com.googlecode.funclate.json.grammar.Grammar;
 import com.googlecode.totallylazy.Callable1;
-import com.googlecode.totallylazy.Function1;
+import com.googlecode.totallylazy.Mapper;
 
 import java.util.Map;
 
@@ -15,29 +15,21 @@ public class Json {
     public static final String SEPARATOR = ",";
 
     public static String toJson(Object value) {
-        if (value == null) return String.valueOf(value);
+        if (value == null) return "null";
         if (value instanceof String) return quote(escape((String) value));
         if (value instanceof Map) return toObjectLiteral((Map) value);
+        if (value instanceof Model) return toObjectLiteral(((Model) value).toMap());
         if (value instanceof Iterable) return toArray((Iterable) value);
-        if (value instanceof Boolean) return String.valueOf(value);
-        if (value instanceof Number) return String.valueOf(value);
-        if (value instanceof Model) return String.valueOf(value);
-        return quote(escape(String.valueOf(value)));
+        if (value instanceof Boolean) return value.toString();
+        if (value instanceof Number) return value.toString();
+        return quote(escape(value.toString()));
     }
 
     public static String escape(String value) {
-        return characters(value).map(escape()).toString("");
+        return characters(value).map(functions.escape).toString("");
     }
 
-    private static Function1<Character, String> escape() {
-        return new Function1<Character, String>() {
-            public String call(Character character) throws Exception {
-                return escape(character);
-            }
-        };
-    }
-
-    private static String escape(Character character) {
+    public static String escape(Character character) {
         switch (character) {
             case '"': return "\\\"";
             case '\\': return "\\\\";
@@ -57,20 +49,12 @@ public class Json {
         return format("%s:%s", quote(String.valueOf(key)), toJson(value));
     }
 
-    public static String toArray(Iterable values) {
-        return sequence(values).map(toJson()).toString("[", SEPARATOR, "]");
+    public static String toArray(Iterable<?> values) {
+        return sequence(values).map(functions.toJson).toString("[", SEPARATOR, "]");
     }
 
-    public static String toObjectLiteral(Map map) {
-        return sequence(map.entrySet()).map(asString()).toString("{", SEPARATOR, "}");
-    }
-
-    public static Callable1<? super Map.Entry, String> asString() {
-        return new Callable1<Map.Entry, String>() {
-            public String call(Map.Entry entry) throws Exception {
-                return toPair(entry.getKey(), entry.getValue());
-            }
-        };
+    public static String toObjectLiteral(Map<?, ?> map) {
+        return sequence(map.entrySet()).map(functions.toPair).toString("{", SEPARATOR, "}");
     }
 
     @Deprecated //use functions.toJson() removed after version 182
@@ -86,21 +70,29 @@ public class Json {
         return Grammar.OBJECT.parse(json);
     }
 
-    public class functions {
-        public Function1<String, Map<String, Object>> parse() {
-            return new Function1<String, Map<String, Object>>() {
-                public Map<String, Object> call(String json) throws Exception {
-                    return Json.parse(json);
-                }
-            };
-        }
+    public static class functions {
+        public static Mapper<String, Map<String, Object>> parse = new Mapper<String, Map<String, Object>>() {
+            public Map<String, Object> call(String json) throws Exception {
+                return Json.parse(json);
+            }
+        };
 
-        public Function1<Object, String> toJson() {
-            return new Function1<Object, String>() {
-                public String call(Object value) throws Exception {
-                    return Json.toJson(value);
-                }
-            };
-        }
+        public static Mapper<Object, String> toJson = new Mapper<Object, String>() {
+            public String call(Object value) throws Exception {
+                return Json.toJson(value);
+            }
+        };
+
+        public static Mapper<Character, String> escape = new Mapper<Character, String>() {
+            public String call(Character character) throws Exception {
+                return escape(character);
+            }
+        };
+
+        public static Mapper<Map.Entry, String> toPair = new Mapper<Map.Entry, String>() {
+            public String call(Map.Entry entry) throws Exception {
+                return toPair(entry.getKey(), entry.getValue());
+            }
+        };
     }
 }
