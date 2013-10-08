9b0bfc203c80b2c55c9695690a7775ade614bc71
Daniel Worthington-Bodart <dan@bodar.com>
2013-10-08 16:18:27 +0100

    Switched to using multimethod support

--- src/com/googlecode/funclate/json/StreamingJson.java
+++ src/com/googlecode/funclate/json/StreamingJson.java
@@ -3,10 +3,13 @@ package com.googlecode.funclate.json;
 import com.googlecode.funclate.Model;
 import com.googlecode.totallylazy.Iterators;
 import com.googlecode.totallylazy.Mapper;
+import com.googlecode.totallylazy.annotations.multimethod;
+import com.googlecode.totallylazy.multi;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 import static com.googlecode.totallylazy.Appendables.append;
 import static com.googlecode.totallylazy.LazyException.lazyException;
@@ -15,50 +18,55 @@ public class StreamingJson {
     public static final String SEPARATOR = ",";
 
     public static <A extends Appendable> A toJson(final Object o, final A appendable) {
-        if (o == null) return toJson((Void) null, appendable);
-        if (o instanceof CharSequence) return toJson((CharSequence) o, appendable);
-        if (o instanceof Number) return toJson((Number) o, appendable);
-        if (o instanceof Boolean) return toJson((Boolean) o, appendable);
-        if (o instanceof Map) return toJson((Map) o, appendable);
-        if (o instanceof Map.Entry) return toJson((Map.Entry) o, appendable);
-        if (o instanceof Iterator) return toJson((Iterator) o, appendable);
-        if (o instanceof Iterable) return toJson(((Iterable) o), appendable);
-        if (o instanceof Model) return toJson(((Model) o), appendable);
-        return toJson(o.toString(), appendable);
+        return new multi(){}.<A>methodOption(o, appendable).getOrElse(new Callable<A>() {
+            @Override
+            public A call() throws Exception {
+                return toJson(o.toString(), appendable);
+            }
+        });
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final CharSequence charSequence, final A appendable) {
         return append(Strings.toString(charSequence), appendable);
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Iterator<?> iterator, final A appendable) {
         return iterate(iterator, appendable, "[", SEPARATOR, "]");
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Iterable<?> iterable, final A appendable) {
         return toJson(iterable.iterator(), appendable);
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Model model, final A appendable) {
         return toJson(model.toMap(), appendable);
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Map<?, ?> map, final A appendable) {
         return iterate(map.entrySet().iterator(), appendable, "{", SEPARATOR, "}");
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Map.Entry<?, ?> entry, final A appendable) {
         return toJson(entry.getValue(), append(':', append(Strings.toString(String.valueOf(entry.getKey())), appendable)));
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Void aVoid, final A appendable) {
         return append("null", appendable);
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Number number, final A appendable) {
         return append(number.toString(), appendable);
     }
 
+    @multimethod
     public static <A extends Appendable> A toJson(final Boolean bool, final A appendable) {
         return append(bool.toString(), appendable);
     }
