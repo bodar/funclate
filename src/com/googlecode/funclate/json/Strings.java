bd25ed1f764fc57d4bb91fd799f80577bcf79596
Daniel Worthington-Bodart <dan@bodar.com>
2013-10-08 15:44:16 +0100

    Moved String related behaviour into one place

--- src/com/googlecode/funclate/json/Strings.java
+++ src/com/googlecode/funclate/json/Strings.java
@@ -3,6 +3,7 @@ package com.googlecode.funclate.json;
 import com.googlecode.totallylazy.Mapper;
 
 import static com.googlecode.totallylazy.Sequences.characters;
+import static java.lang.Integer.parseInt;
 import static java.lang.String.format;
 
 public class Strings {
@@ -30,6 +31,20 @@ public class Strings {
         return format("\"%s\"", value);
     }
 
+    public static String unescape(String escaped) {
+        switch (escaped.charAt(1)) {
+            case '"': return "\"";
+            case '\\': return "\\";
+            case '/': return "/";
+            case 'b': return "\b";
+            case 'n': return "\n";
+            case 'r': return "\r";
+            case 't': return "\t";
+            case 'f': return "\f";
+            case 'u': return Character.toString((char) parseInt(escaped.substring(2), 16));
+            default: throw new UnsupportedOperationException();
+        }
+    }
 
     public static class functions {
         public static Mapper<Character, String> escape = new Mapper<Character, String>() {
@@ -37,6 +52,11 @@ public class Strings {
                 return Strings.escape(character);
             }
         };
-    }
 
+        public static Mapper<String, String> unescape = new Mapper<String, String>() {
+                public String call(String escaped) throws Exception {
+                    return Strings.unescape(escaped);
+                }
+            };
+    }
 }
