15969bf99871a7e7e80b8b7e39e4c67485beaaa2
Stuart & Enrico <Stuart & Enrico@localhost>
2014-04-30 14:55:48 +0100

    Added helper functions to Renderer interface

--- src/com/googlecode/funclate/Renderer.java
+++ src/com/googlecode/funclate/Renderer.java
@@ -1,15 +1,41 @@
 package com.googlecode.funclate;
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Function2;
 import com.googlecode.totallylazy.Mapper;
+
+import static com.googlecode.totallylazy.Callables.flip;
+import static com.googlecode.totallylazy.Callers.call;
+import static com.googlecode.totallylazy.Mapper.mapper;
 
 public interface Renderer<T> {
     String render(T instance) throws Exception;
 
+    class constructors {
+        public static <T> Renderer<T> renderer(final Callable1<T, String> callable) {
+            return new Renderer<T>() {
+                @Override
+                public String render(T t) throws Exception {
+                    return callable.call(t);
+                }
+            };
+        }
+    }
+
     class functions {
         public static <T> Mapper<Renderer<T>, String> render(final T instance) {
-            return new Mapper<Renderer<T>, String>() {
-                public String call(Renderer<T> renderer) throws Exception {
-                    return renderer.render(instance);
+            return mapper(call(functions.<T>render(), instance));
+        }
+
+        public static <T> Mapper<T, String> render(final Renderer<T> renderer) {
+            return mapper(call(flip(functions.<T>render()), renderer));
+        }
+
+        public static <T> Function2<T, Renderer<T>, String> render() {
+            return new Function2<T, Renderer<T>, String>() {
+                @Override
+                public String call(T t, Renderer<T> renderer) throws Exception {
+                    return renderer.render(t);
                 }
             };
         }
